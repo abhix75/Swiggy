c9e3241ef2bd6065551f5c3855769bd6721ab9fa
Abhijit Mishra <abhijitmishraak10@gmail.com>
2023-12-20 18:00:15 +0530

    episode-5(structured files in folder)

---
+++ src/components/Body.js
@@ -0,0 +1,18 @@
+import ResList from "../Utils/reslist";
+import ResturantCard from "./RestaurantCard";
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                {
+                    ResList.restaurants.map((data) => (
+                        <ResturantCard key={data.info.id} resData={data} />
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Body;
