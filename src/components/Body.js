670b757c690de3292814d3c102ce917555552faa
Abhijit Mishra <abhijitmishraak10@gmail.com>
2023-12-21 12:41:38 +0530

    epsiode-6(useEffect)swiggyApiUsed

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,22 @@
 import ResList from "../Utils/reslist";
 import ResturantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 const Body = () => {
 
     const [ListofRestaurant,setListofRestaurant]=useState(ResList.restaurants);
+
+    useEffect(()=>{
+    fetchData();
+    },[]);
+    
+    const fetchData = async ()=>{
+        const data = await fetch(
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+            );
+        const json = await data.json();
+        console.log(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setListofRestaurant(json?.data?.cards[2]?.card.card?.gridElements?.infoWithStyle?.restaurants);
+    }
     return (
         <div className="body">
             <div className="filter">
