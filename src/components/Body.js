413828422124413d05fa041df49393c8ccdc346c
Abhijit Mishra <abhijitmishraak10@gmail.com>
2023-12-22 10:54:20 +0530

    episode-6(incude search)

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,32 +4,57 @@ import { useEffect, useState } from "react";
 import ShimmerUI from "./shimmerUI";
 const Body = () => {
 
-    const [ListofRestaurant,setListofRestaurant]=useState([]);
-    
-    useEffect(()=>{
-    fetchData();
-    },[]);
-    
-    const fetchData = async ()=>{
+    const [ListofRestaurant, setListofRestaurant] = useState([]);
+    const [SearchText, setSearchText] = useState("");
+    console.log("render")
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
         const data = await fetch(
             "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-            );
+        );
         const json = await data.json();
-        console.log(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-        setListofRestaurant(json?.data?.cards[2]?.card.card?.gridElements?.infoWithStyle?.restaurants);
+        console.log(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setListofRestaurant(json?.data?.cards[1]?.card.card?.gridElements?.infoWithStyle?.restaurants);
     }
 
-  
-    return ListofRestaurant.length === 0 ? (<ShimmerUI/>):(
+
+    return ListofRestaurant.length === 0 ? (<ShimmerUI />) : (
         <div className="body">
             <div className="filter">
+            <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={SearchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              console.log(SearchText);
+              const filteredRestaurant = ListofRestaurant.filter((res) =>
+                res.info.name.toLowerCase().includes(SearchText.toLowerCase())
+              );
+              console.log(filteredRestaurant)
+              setListofRestaurant(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+
+                </div>
                 <button className="filter-btn"
-                           onClick={() => {
-                           const filteredList = ListofRestaurant.filter(
+                    onClick={() => {
+                        const filteredList = ListofRestaurant.filter(
                             (res) => res.info.avgRating > 4.5
-            );
-            setListofRestaurant(filteredList);
-          }}>Top Rated restaurant</button>
+                        );
+                        console.log("filteredList",filteredList);
+                        setListofRestaurant(filteredList);
+                    }}>Top Rated restaurant</button>
             </div>
             <div className="res-container">
                 {
