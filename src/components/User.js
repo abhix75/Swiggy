e180cb8dec9414fb0e914a2d914db9eee54832d4
Abhijit Mishra <abhijitmishraak10@gmail.com>
2024-01-06 11:15:22 +0530

    state-varibale(class vs function)

--- src/components/User.js
+++ src/components/User.js
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import { useState } from 'react'
 const User = (props) => {
+
+    const [count,setcount] = useState(0);
+    const [count2,setcount2] = useState(1);
   return (
     <div className='user-card'>
+       <h2>Count:{count}</h2>
+       <h2>Count2:{count2}</h2>
        <h2>Name: {props.name}</h2>
        <h3>Location: {props.location}</h3>    
     </div>
