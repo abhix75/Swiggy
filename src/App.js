560bd466fb536b2465b67e6f62551ed19a4deebf
Abhijit Mishra <abhijitmishraak10@gmail.com>
2024-01-06 16:44:28 +0530

    implemented Lazy Loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{Suspense, lazy} from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
@@ -6,6 +6,7 @@ import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import RestaurantMenu from "./components/RestaurantMenu";
+const Grocery = lazy(()=>import("./components/Grocery"))
 const AppLayout = () => {
     return (
         <div className="app">
@@ -31,6 +32,10 @@ const appRoute = createBrowserRouter([
             {
                 path:"/contact",
                 element:<Contact/>
+            },
+            {
+                path:"/grocery",
+                element:<Suspense fallback={<h1>Loading....</h1>}><Grocery/></Suspense>
             },
             {
                 path:"/restaurants/:resId",
