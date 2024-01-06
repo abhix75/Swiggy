f703c30b578e72186ba917697c02fe8a976642df
Abhijit Mishra <abhijitmishraak10@gmail.com>
2024-01-06 11:29:35 +0530

    how to change state variable in class based component

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,9 @@
-import User from "./User";
 import UserClass from "./User-Class";
 const About = () => {
     return (
         <div>
             <h1>About Me</h1>
             <h2>WELCOME TO ABHIJIT FOOD CLUB</h2>
-            <User name={"Abhijit Mishra (Function)"} location={"Nuapada(Function)"}/>
             <UserClass name={"Abhijit Mishra (Class)"} location={"Nuapada(Class)"} />
         </div>
     )
--- src/components/User-Class.js
+++ src/components/User-Class.js
@@ -1,28 +1,35 @@
 import React from "react";
 
-class UserClass extends React.Component{
+class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
 
-    constructor(props){
-        super(props)
-        console.log(props)
+    this.state = {
+      count: 1,
+      count2: 2,
+    };
+  }
+  render() {
+    const { name, location } = this.props;
+    return (
+      <div className="user-card">
+        <h2>Count:{this.state.count}</h2>
 
-        this.state = {
-            count : 1,
-            count2 : 2
-        }
-    }
-    render(){
-        const {name,location}=this.props
-        return (
-            <div className='user-card'>
-               <h2>Count:{this.state.count}</h2>
-               <h2>Count2:{this.state.count2}</h2>
+        <button
+          onClick={() => {
+            this.setState({ count: this.state.count + 1 });
+          }}
+        >
+          Count
+        </button>
+        <h2>Count2:{this.state.count2}</h2>
 
-               <h2>Name: {name}</h2>
-               <h3>Location:{location}</h3>    
-            </div>
-          )
-    }
+        <h2>Name: {name}</h2>
+        <h3>Location:{location}</h3>
+      </div>
+    );
+  }
 }
 
-export default UserClass
+export default UserClass;
